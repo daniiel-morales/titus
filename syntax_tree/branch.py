9c95d7b2e0ecc8282dda0bab594f56ed589ee73c
daniiel-morales <daniiel.morales@outlook.com>
2020-06-10 23:59:20 -0600

    add +,-,*,/,and,or,xor,assing node execute

--- syntax_tree/branch.py
+++ syntax_tree/branch.py
@@ -1,6 +1,15 @@
+#▓█████▄  ▄▄▄       ███▄    █ ▒██░ ██░ ███▄ ▄███░ ▒█████   ██▀███  
+#▒██▀ ██▌▒████▄     ██ ▀█   █ ▒██▒▒██▒▓██▒▀█▀ ██▒▒██▒  ██▒▓██ ▒ ██▒
+#░██   █▌▒██  ▀█▄  ▓██  ▀█ ██▒▒██▒▒██▒▓██    ▓██░▒██░  ██▒▓██ ░▄█ ▒
+#░▓█▄   ▌░██▄▄▄▄██ ▓██▒  ▐▌██▒░██░░██░▒██    ▒██ ▒██   ██░▒██▀▀█▄  
+#░▒████▓  ▓█   ▓██▒▒██░   ▓██░░██░░██░▒██▒   ░██▒░ ████▓▒░░██▓ ▒██▒
+# ▒▒▓  ▒  ▒▒   ▓▒█░░ ▒░   ▒ ▒ ░▓  ░▓  ░ ▒░   ░  ░░ ▒░▒░▒░ ░ ▒▓ ░▒▓░
+# ░ ▒  ▒   ▒   ▒▒ ░░ ░░   ░ ▒░ ▒ ░ ▒ ░░  ░      ░  ░ ▒ ▒░   ░▒ ░ ▒░
+# ░ ░  ░   ░   ▒      ░   ░ ░  ▒ ░ ▒ ░░      ░   ░ ░ ░ ▒    ░░   ░ 
+#   ░          ░  ░         ░  ░   ░         ░       ░ ░     ░     
 # pylint: disable=W,import-error
 from syntax_tree.node import node
-
+import syntax_tree.calculate as operate
 class branch(node):
     # child amount
     __counter = 0
@@ -38,4 +47,30 @@ class branch(node):
 
     def execute(self, sym_table):
         #TODO switch for execute each node.TYPE of branch
-        pass
+        switch={
+                # MATH
+                self.TYPE["ADD"] : operate.ADD,
+                self.TYPE["SUB"] : operate.SUB,
+                self.TYPE["MUL"] : operate.MUL,
+                self.TYPE["DIV"] : operate.DIV,
+                self.TYPE["MOD"] : operate.MOD,
+                # LOGIC
+                self.TYPE["AND"] : operate.AND,
+                self.TYPE["OR"] : operate.OR,
+                self.TYPE["XOR"] : operate.XOR,
+
+                self.TYPE["ASSIGN"] : operate.ASSIGN
+                }
+        #TODO need to return semantic errors and print to terminal
+        for label in range(self.getSize()):
+            label_node = self.getChild(label)
+            if label_node.getType() == self.TYPE["LABEL"]:
+                for child in range(label_node.getSize()):
+                    child_node = label_node.getChild(child)
+                    func=switch.get(child_node.getType(),lambda :'Node not defined')
+                    result = func(child_node, sym_table)
+                    if result != None:
+                        return result
+            else:
+                func=switch.get(self.getType(),lambda :'Node not defined')
+                return func(self, sym_table)
