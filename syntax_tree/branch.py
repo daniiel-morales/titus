ed59074f045d94557ef3686806c3d7c5781a48eb
daniiel-morales <daniiel.morales@outlook.com>
2020-06-15 17:36:36 -0600

    add goto and entry for bitwise, abs, unset, not

--- syntax_tree/branch.py
+++ syntax_tree/branch.py
@@ -45,50 +45,87 @@ class branch(node):
             return self._value[index]
         return None
 
-    def execute(self, sym_table):
-        #TODO switch for execute each node.TYPE of branch
-        switch={
-                # MATH
-                self.TYPE["ADD"] : operate.ADD,
-                self.TYPE["SUB"] : operate.SUB,
-                self.TYPE["MUL"] : operate.MUL,
-                self.TYPE["DIV"] : operate.DIV,
-                self.TYPE["MOD"] : operate.MOD,
-                # LOGIC
-                self.TYPE["AND"] : operate.AND,
-                self.TYPE["OR"] : operate.OR,
-                self.TYPE["XOR"] : operate.XOR,
-                # COMPARE
-                self.TYPE["EQUAL"] : operate.EQUAL,
-                self.TYPE["NOEQUAL"] : operate.NOEQUAL,
-                self.TYPE["GTHAN"] : operate.GTHAN,
-                self.TYPE["GE_OP"] : operate.GE_OP,
-                self.TYPE["LTHAN"] : operate.LTHAN,
-                self.TYPE["LE_OP"] : operate.LE_OP,
-                # STRUCT
-                self.TYPE["ACCESS"] : operate.ACCESS,
-
-                self.TYPE["PRINT"] : operate.PRINT,
-                self.TYPE["ASSIGN"] : operate.ASSIGN
-                }
-        #TODO need to return semantic errors
-        has_print = False
+    def start_execute(self, sym_table, start_from):
+        delete_label_up_to = 0
         for label in range(self.getSize()):
             label_node = self.getChild(label)
+            if label_node.getValue() == start_from:
+                break
+            else:
+                delete_label_up_to += 1
+
+        # pop labels to make a goto simulation
+        for label in range(delete_label_up_to):
+            self._value.pop(label)
+
+        return self.execute(sym_table)
+
+    switch={
+            # MATH
+            node.TYPE["ADD"] : operate.ADD,
+            node.TYPE["SUB"] : operate.SUB,
+            node.TYPE["MUL"] : operate.MUL,
+            node.TYPE["DIV"] : operate.DIV,
+            node.TYPE["MOD"] : operate.MOD,
+
+            node.TYPE["ABS"] : operate.ABS,
+            # LOGIC
+            node.TYPE["AND"] : operate.AND,
+            node.TYPE["OR"]  : operate.OR,
+            node.TYPE["XOR"] : operate.XOR,
+
+            node.TYPE["NOT"] : operate.NOT,
+            # COMPARE
+            node.TYPE["EQUAL"]  : operate.EQUAL,
+            node.TYPE["NOEQUAL"]: operate.NOEQUAL,
+            node.TYPE["GTHAN"]  : operate.GTHAN,
+            node.TYPE["GE_OP"]  : operate.GE_OP,
+            node.TYPE["LTHAN"]  : operate.LTHAN,
+            node.TYPE["LE_OP"]  : operate.LE_OP,
+            # BITWISE
+            node.TYPE["BAND"]   : operate.BAND,
+            node.TYPE["BOR"]    : operate.BOR,
+            node.TYPE["BXOR"]   : operate.BXOR,
+            node.TYPE["SLEFT"]  : operate.SLEFT,
+            node.TYPE["SRIGHT"] : operate.SRIGHT,
+
+            node.TYPE["BNOT"]   : operate.BNOT,
+            # STRUCT
+            node.TYPE["ACCESS"] : operate.ACCESS,
+            
+            node.TYPE["UNSET"]  : operate.UNSET,
+            node.TYPE["PRINT"]  : operate.PRINT,
+            node.TYPE["ASSIGN"] : operate.ASSIGN
+            }
+
+    def execute(self, sym_table):
+        #TODO need to return semantic errors
+        has_print = False
+        label_list = list(self._value.keys())
+        for label in label_list:
+            label_node = self._value[label]
             if label_node.getType() == self.TYPE["LABEL"]:
                 for child in range(label_node.getSize()):
                     child_node = label_node.getChild(child)
-                    func=switch.get(child_node.getType(),lambda :'Node not defined')
-                    result = func(child_node, sym_table)
-                    if child_node.getType() == self.TYPE["PRINT"]:
-                        has_print = True
-                    # save the ast node in reference for goto
-                    label_ref = sym_table.get(label_node.getValue())
-                    label_ref.setRef(label_node)
-                    sym_table.update(label_node.getValue(), label_ref)
+                    if child_node.getType() == self.TYPE["EXIT"]:
+                        if has_print:
+                            return [sym_table.getLog(), None]
+                        return [None, None]
+                    elif child_node.getType() != self.TYPE["GOTO"]:
+                        func=self.switch.get(child_node.getType(),lambda :'Node not defined')
+                        result = func(child_node, sym_table)
+                        if child_node.getType() == self.TYPE["PRINT"]:
+                            has_print = True
+                    else:
+                        # need to finish the execution
+                        # and return a value for restart execution for other point
+                        goto_label = child_node.getChild(0).getValue()
+                        if has_print:
+                            return [sym_table.getLog(), goto_label]
+                        return [None, goto_label]
             else:
-                func=switch.get(self.getType(),lambda :'Node not defined')
+                func=self.switch.get(self.getType(),lambda :'Node not defined')
                 return func(self, sym_table)
         if has_print:
-            return sym_table.getLog()
-        return None
+            return [sym_table.getLog(), None]
+        return [None,None]
